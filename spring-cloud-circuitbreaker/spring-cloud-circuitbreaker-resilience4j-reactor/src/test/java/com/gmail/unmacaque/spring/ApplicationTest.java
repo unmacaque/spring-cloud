62855492ce7efd5e8395dc4727fd955c4ead7e8f
Andreas Trepczik <andy@asrock.fritz.box>
2019-12-17 15:03:54 +0100

    add spring-cloud-circuitbreaker-resilience4j-reactor

---
+++ spring-cloud-circuitbreaker/spring-cloud-circuitbreaker-resilience4j-reactor/src/test/java/com/gmail/unmacaque/spring/ApplicationTest.java
@@ -0,0 +1,37 @@
+package com.gmail.unmacaque.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class ApplicationTest {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void testHello() {
+		webTestClient
+				.get()
+				.uri("/")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("hello");
+	}
+
+	@Test
+	void testHelloWithFault() {
+		webTestClient
+				.get()
+				.uri("/")
+				.header("X-Provoke-Fault", "1")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("fallback");
+	}
+
+}
