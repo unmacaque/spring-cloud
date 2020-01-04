e27c4688abcbdb27691a87f433fed84eea75411d
Andreas Trepczik <andy@asrock.fritz.box>
2020-01-04 09:42:43 +0100

    spring-cloud-function-stream: use test binder

--- spring-cloud-function/spring-cloud-function-stream/src/main/java/com/gmail/unmacaque/spring/Application.java
+++ spring-cloud-function/spring-cloud-function-stream/src/main/java/com/gmail/unmacaque/spring/Application.java
@@ -2,14 +2,11 @@ package com.gmail.unmacaque.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.context.annotation.Bean;
 
 import java.util.function.Function;
 
 @SpringBootApplication
-@EnableBinding(Processor.class)
 public class Application {
 
 	public static void main(String[] args) {
--- spring-cloud-function/spring-cloud-function-stream/src/test/java/com/gmail/unmacaque/spring/ApplicationTest.java
+++ spring-cloud-function/spring-cloud-function-stream/src/test/java/com/gmail/unmacaque/spring/ApplicationTest.java
@@ -2,28 +2,30 @@ package com.gmail.unmacaque.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
-import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.cloud.stream.test.binder.MessageCollector;
-import org.springframework.messaging.Message;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.messaging.support.MessageBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@FunctionalSpringBootTest
+@SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
 class ApplicationTest {
 
 	@Autowired
-	private Processor processor;
+	private InputDestination input;
 
 	@Autowired
-	private MessageCollector messageCollector;
+	private OutputDestination output;
 
 	@Test
 	void messageIsProcessed() {
-		processor.input().send(MessageBuilder.withPayload("it works").build());
-		Message<?> received = messageCollector.forChannel(processor.output()).poll();
-		assertThat(received.getPayload()).isEqualTo("IT WORKS");
+		input.send(MessageBuilder.withPayload("it works").build());
+		String message = new String(output.receive().getPayload());
+		assertThat(message).isEqualTo("IT WORKS");
 	}
 
 }
