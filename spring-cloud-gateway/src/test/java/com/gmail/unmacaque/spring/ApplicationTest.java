27a220ee0ba63a0e5f44cb85343b933295b49ae0
Andreas Trepczik <andy@asrock.fritz.box>
2020-01-04 12:25:27 +0100

    add spring-cloud-gateway

---
+++ spring-cloud-gateway/src/test/java/com/gmail/unmacaque/spring/ApplicationTest.java
@@ -0,0 +1,35 @@
+package com.gmail.unmacaque.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"endpoint=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock
+class ApplicationTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	void testRouteApi() {
+		stubFor(get("/api").willReturn(aResponse().withBody("Api called!")));
+
+		WebTestClient
+				.bindToServer()
+				.baseUrl("http://localhost:" + port)
+				.build()
+				.get()
+				.uri("/api")
+				.exchange()
+				.expectStatus().isOk();
+
+		verify(getRequestedFor(urlEqualTo("/api")));
+	}
+
+}
