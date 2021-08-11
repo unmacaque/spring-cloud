c9b4f39b930599ee779612336ac152ca79526cc0
Andreas Trepczik <andy@asrock.fritz.box>
2021-08-11 16:48:45 +0200

    improve tests

--- spring-cloud-openfeign/src/test/java/com/gmail/unmacaque/spring/domain/ProductControllerTest.java
+++ spring-cloud-openfeign/src/test/java/com/gmail/unmacaque/spring/domain/ProductControllerTest.java
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@SpringBootTest
 @AutoConfigureMockMvc
 class ProductControllerTest {
 
@@ -22,6 +21,7 @@ class ProductControllerTest {
 	void testProducts() throws Exception {
 		mvc.perform(get("/products"))
 				.andExpect(status().isOk())
-				.andExpect(content().json("[]"));
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$.length()").value(0));
 	}
 }
--- spring-cloud-sleuth/src/test/java/com/gmail/unmacaque/spring/domain/WebControllerTest.java
+++ spring-cloud-sleuth/src/test/java/com/gmail/unmacaque/spring/domain/WebControllerTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@SpringBootTest
 @AutoConfigureMockMvc
 class WebControllerTest {
 
@@ -20,6 +20,7 @@ class WebControllerTest {
 	@Test
 	void testProducts() throws Exception {
 		mvc.perform(get("/"))
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello World"));
 	}
 }
