7ba96dbe00049a37e1510548d78213130a663174
Andreas Trepczik <andy@asrock.fritz.box>
2021-09-16 18:15:16 +0200

    rename packages, remove redundant dependency

--- spring-cloud-circuitbreaker/spring-cloud-circuitbreaker-resilience4j/src/main/java/com/gmail/unmacaque/spring/config/CircuitBreakerConfiguration.java
+++ spring-cloud-circuitbreaker/spring-cloud-circuitbreaker-resilience4j/src/main/java/com/gmail/unmacaque/spring/config/CircuitBreakerConfiguration.java
@@ -1,8 +1,8 @@
-package com.gmail.unmacaque.spring.domain;
+package com.gmail.unmacaque.spring.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +14,7 @@ import java.time.Duration;
 public class CircuitBreakerConfiguration {
 
 	@Bean
-	public Customizer<ReactiveResilience4JCircuitBreakerFactory> customizer() {
+	public Customizer<Resilience4JCircuitBreakerFactory> customizer() {
 		return factory -> factory
 				.configureDefault(id -> new Resilience4JConfigBuilder(id)
 						.circuitBreakerConfig(CircuitBreakerConfig
