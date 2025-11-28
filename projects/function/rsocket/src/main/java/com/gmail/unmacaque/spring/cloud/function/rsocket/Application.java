f565ff56a8a86e2663d513caaada940500a48db4
Andreas Trepczik <andy@home.lab>
2025-11-28 09:22:16 +0100

    remove unnecessary public modifier

--- projects/function/rsocket/src/main/java/com/gmail/unmacaque/spring/cloud/function/rsocket/Application.java
+++ projects/function/rsocket/src/main/java/com/gmail/unmacaque/spring/cloud/function/rsocket/Application.java
@@ -9,7 +9,7 @@ import java.util.function.Function;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
--- projects/function/webflux/src/main/java/com/gmail/unmacaque/spring/cloud/function/webflux/Application.java
+++ projects/function/webflux/src/main/java/com/gmail/unmacaque/spring/cloud/function/webflux/Application.java
@@ -17,7 +17,7 @@ public class Application {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
